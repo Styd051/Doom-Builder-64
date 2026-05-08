3df395e8a8339ce0919c7ec9cd83ad6434252ee4
Styd051 <thierus.dylan@gmail.com>
2026-05-08 16:18:16 +0200

    @ Removed some console output code that was used for testing
    @ Gave the ThingTypeInfo a string representation (ToString) by codeimp https://sourceforge.net/p/doombuilder/code/1316/

--- Source/Core/Config/ThingTypeInfo.cs
+++ Source/Core/Config/ThingTypeInfo.cs
@@ -305,6 +305,12 @@ namespace CodeImp.DoomBuilder.Config
 		{
 			return string.Compare(this.title, other.title, true);
 		}
+
+		// String representation
+		public override string ToString()
+		{
+			return this.title + " (" + index + ")";
+		}
 		
 		#endregion
 	}
