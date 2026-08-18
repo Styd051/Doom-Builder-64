0839d71f5bf2e221ffa4c24f70cfff9471a435ed
Styd051 <thierus.dylan@gmail.com>
2026-08-18 06:38:25 +0200

    Better timing clock without accuracy loss after long system uptime (should solve camera movement lag issues) by codeimp https://sourceforge.net/p/doombuilder/code/1581/

--- Source/Core/General/Clock.cs
+++ Source/Core/General/Clock.cs
@@ -1,20 +1,8 @@
+#if WINDOWS
+#define USE_GETTIME
+#endif
 
 #region ================== Copyright (c) 2007 Pascal vd Heiden
-
-/*
- * Copyright (c) 2007 Pascal vd Heiden, www.codeimp.com
- * This program is released under GNU General Public License
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- */
-
-#endregion
-
-#region ================== Namespaces
 
 using System;
 using System.Collections;
@@ -30,55 +18,52 @@ namespace CodeImp.DoomBuilder
 {
 	public class Clock
 	{
-        #region ================== Declarations
+		#region ================== Declarations
+		
+		#if USE_GETTIME
 
-        //#if !LINUX
+		[DllImport("winmm.dll")]
+		private static extern uint timeGetTime();
+		
+		#endif
 
-        [DllImport("kernel32.dll")]
-        private static extern short QueryPerformanceCounter(out long x);
+		#endregion
+		
+		#region ================== Constants
 
-        [DllImport("kernel32.dll")]
-        private static extern short QueryPerformanceFrequency(out long x);
+		#endregion
 
-        //#endif
+		#region ================== Variables
 
-        #endregion
+		// Settings
+		private uint lasttime;
+		private uint currenttime;
+		
+		// Disposing
+		private bool isdisposed = false;
 
-        #region ================== Constants
+		#endregion
 
-        #endregion
+		#region ================== Properties
 
-        #region ================== Variables
+		// Settings
+		public double CurrentTime { get { return currenttime; } }
 
-        // Settings
-        private double currenttime;
+		#endregion
 
-        // Disposing
-        private bool isdisposed = false;
+		#region ================== Constructor / Disposer
 
-        #endregion
-
-        #region ================== Properties
-
-        // Settings
-        public double CurrentTime { get { return currenttime; } }
-
-        // Disposing
-        public bool IsDisposed { get { return isdisposed; } }
-
-        #endregion
-
-        #region ================== Constructor / Disposer
-
-        // Constructor
-        public Clock()
-        {
-            // We have no destructor
-            GC.SuppressFinalize(this);
-        }
-
-        // Disposer
-        public void Dispose()
+		// Constructor
+		public Clock()
+		{
+			GetCurrentTime();
+			
+			// We have no destructor
+			GC.SuppressFinalize(this);
+		}
+		
+		// Disposer
+		public void Dispose()
 		{
 			// Not already disposed?
 			if(!isdisposed)
@@ -94,44 +79,42 @@ namespace CodeImp.DoomBuilder
 
 		#region ================== Methods
 
+		// This returns the current time in milliseconds as a uint
+		private static uint GetTime()
+		{
+			#if USE_GETTIME
+
+			// Use Windows' timeGetTime
+			return timeGetTime();
+			
+			#else
+			
+			// Use standard clock
+			return unchecked((uint)Environment.TickCount);
+			
+			#endif
+		}
+		
 		// This queries the system for the current time
 		public double GetCurrentTime()
 		{
-            // Only windows has QPC
-            //#if !LINUX
+			// Get the current system time
+			uint nexttime = GetTime();
 
-            long timefrequency;
+			// Determine delta time since previous update
+			// (this takes care of time wrapping around to 0)
+			uint deltatime;
+			if(nexttime < lasttime)
+				deltatime = (uint.MaxValue - lasttime) + nexttime;
+			else
+				deltatime = nexttime - lasttime;
 
-            // Get the high resolution clock frequency
-            if (QueryPerformanceFrequency(out timefrequency) == 0)
-            {
-                // No high resolution clock available
-                currenttime = (double)Environment.TickCount;
-            }
-            else
-            {
-                long timecount;
+			// Add the elapsed time to our internal time
+			currenttime += deltatime;
 
-                // Get the high resolution count
-                QueryPerformanceCounter(out timecount);
+			lasttime = nexttime;
 
-                // Calculate high resolution time in milliseconds
-                // TODO: It seems there is a loss of precision here when the
-                // result of this math is assigned to currenttime, WHY?!
-                currenttime = (double)timecount / (double)timefrequency * (double)1000.0;
-            }
-
-            /*
-			#else
-			
-			// In LINUX always use standard clock
-			currenttime = (double)Environment.TickCount;
-			
-			#endif
-			*/
-
-            // Return the current time
-            return currenttime;
+			return currenttime;
 		}
 		
 		#endregion
